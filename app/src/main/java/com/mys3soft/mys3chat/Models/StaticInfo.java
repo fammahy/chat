384488685e29fc5afb974cf260222e63610fe6d4
fammahy <fatimahyusuf1797@yahoo.com>
2021-04-09 19:20:15 +0100

    changed structure

---
+++ app/src/main/java/com/mys3soft/mys3chat/Models/StaticInfo.java
@@ -0,0 +1,18 @@
+package com.mys3soft.mys3chat.Models;
+
+
+public class StaticInfo {
+
+    public static String EndPoint = "https://maryam-6b14a.firebaseio.com";
+    public static String MessagesEndPoint = "https://maryam-6b14a.firebaseio.com/messages";
+    public static String FriendsURL = "https://maryam-6b14a.firebaseio.com/friends";
+    public static String UsersURL = "https://maryam-6b14a.firebaseio.com/users";
+    public static String UserCurrentChatFriendEmail = "";
+    public static String TypingStatus = "TypingStatus";
+
+    public static String NotificationEndPoint = "https://maryam-6b14a.firebaseio.com/notifications";
+    public static String FriendRequestsEndPoint = "https://maryam-6b14a.firebaseio.com/friendrequests";
+
+    public static int ChatAciviityRequestCode = 101;
+
+}
