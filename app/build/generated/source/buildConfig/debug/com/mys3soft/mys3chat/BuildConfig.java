ee3ebb43556dead3a649256817e310172c59a783
fammahy <fatimahyusuf1797@yahoo.com>
2021-04-09 18:33:22 +0100

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/mys3soft/mys3chat/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mys3soft.mys3chat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mys3soft.mys3chat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 28;
+  public static final String VERSION_NAME = "3.8";
+}
